ef6f0e61c3e3b42b4a511edb3d1f46648c7af44a
spookyboogy <matt.tolstoy@gmail.com>
2021-12-02 17:33:12 -0600

    Day 2

--- Day 1/adv01.py
+++ Day 1/adv01.py
@@ -2,12 +2,14 @@ def day1pt1():
 
 	D = [int(i) for i in open('input.txt', 'r').read().splitlines()]
 	return sum([D[i-1] < D[i] for i in range(1, len(D))])
+
 	
 def day1pt2():
 
 	d = [int(i) for i in open('input.txt', 'r').read().splitlines()]
 	D = [d[i] + d[i+1] + d[i+2] for i in range(len(d) - 2)]
 	return sum([D[i-1] < D[i] for i in range(1, len(D))])
+
 
 print(day1pt1())
 print(day1pt2())
